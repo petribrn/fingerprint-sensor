7c8ad481013c9edcac09999e99650c8307049c68
Bruno Petri <brunoopetri@gmail.com>
2022-12-13 19:50:53 -0300

    [INDEX] Updated returned attributes from index

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -22,7 +22,7 @@ class UserController {
 
   async index(req, res) {
     try {
-      const users = await User.findAll({ attributes: ['name', 'fingerprint_id'] });
+      const users = await User.findAll({ attributes: ['name', 'fingerprint_id', 'created_at'] });
       return res.json(users);
     } catch (error) {
       return res.json(null);
